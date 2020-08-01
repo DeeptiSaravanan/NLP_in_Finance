af307649877e65bf11ecc6a503e1ab1a930ddd4d
Ujwal <ujwalnarayann@gmail.com>
2020-08-01 15:14:51 +0530

    Adding outputs

---
+++ enitityRecognition/src/tagAllData.py
@@ -0,0 +1,35 @@
+import os
+import json
+import spacy
+
+from tqdm import tqdm
+
+file_path = './../../data/'
+output_file_path = './../outputs/'
+files = os.listdir(file_path)
+
+nlp = spacy.load('./../models/sebi_ib')
+
+for file in tqdm(files):
+    with open(file_path+file) as handle:
+        data = handle.read().splitlines()
+    output = {}
+    lineNumber = 0
+    for line in data:
+        doc = nlp(line)
+        ents = []
+        entlabels = []
+        for ent in doc.ents:
+            ents.append(ent.text)
+            entlabels.append(ent.label_)
+        output[lineNumber] ={}
+        output[lineNumber]['text'] = line
+        output[lineNumber]['ents'] = ents
+        output[lineNumber]['ent_labels'] = entlabels
+        lineNumber += 1
+    file_name = output_file_path + file[:-7] + 'json'
+    with open(file_name,'w') as handle:
+        json.dump(output, handle)
+
+
+        
