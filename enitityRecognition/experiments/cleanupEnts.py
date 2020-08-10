d82cda97428f62e0e68338cc50bb45f2dd303ce7
Ujwal <ujwalnarayann@gmail.com>
2020-08-10 14:49:01 +0530

    Adding coref code

--- enitityRecognition/experiments/cleanupEnts.py
+++ enitityRecognition/experiments/cleanupEnts.py
@@ -3,19 +3,53 @@ import spacy
 import json
 from tqdm import tqdm
 
-nlp = spacy.load('en')
+# Load data 
+import en_core_web_sm
+nlp = en_core_web_sm.load()
+
 file_path = './'
 
 files = ['insid_dump.json']
 
-for file_name in tqdm(files):
+visited = []
+def mergeEntsFromNounChunks(ent, nounChunks, startPos):
+#     print(ent,nounChunks[startPos])
+    visited.append([ent,startPos])
+    curCount = startPos
+    bigB = ent
+    marked = False
+    possibleAns = []
+    for maybeEnt in nounChunks[startPos:]:
+        if ent in maybeEnt:
+            if bigB in maybeEnt:
+                if len(maybeEnt.split()) - len(ent.split()) < 5:
+                        if len(maybeEnt.split()) > len(bigB.split()):
+                            bigB = maybeEnt
+                            marked = True
+            else:
+#                 print("started", ent, curCount)
+                if( [ent,curCount] not in visited):
+                    possibleAns = mergeEntsFromNounChunks(ent,nounChunks,curCount)
+        curCount +=1
+        
+    if marked:
+        possibleAns.append(bigB)
+#     print(possibleAns)
+    return possibleAns
+                            
+  
+for file_name in (files):
   file = json.load(open(file_path+file_name))
   for i in tqdm(file):
     doc = nlp(file[i]['text'])
     parsedData = []
+    
+    # Parse Data
     for j in doc:
       parsedData.append([j.text,j.pos_])
     file[i]['parsed_data'] = parsedData
+    
+    # Get noun chunks
     file[i]['nounChunks'] = {np.text
     for nc in doc.noun_chunks
     for np in [
@@ -25,25 +59,30 @@ for file_name in tqdm(files):
         :nc.root.right_edge.i+1]]}
     file[i]['nounChunks'] = [i.strip() for i in file[i]['nounChunks']]
 
-  for i in (file):
-    cleanedUpEnts = []
+  # Cleanup entities 
+
+
+                            
+                
+    
+  for i in tqdm(file):
+    cleanedUpEnts = set()
     for ent in file[i]['ents']:
       if ent in file[i]['nounChunks']:
-        cleanedUpEnts.append(ent)
+        cleanedUpEnts.add(ent)
+    
+    # Merge noun chunks 
+    nounChunks = file[i]['nounChunks']
+    # print(i)
+    visited = []
     for ent in file[i]['ents']:
-      if ent not in cleanedUpEnts:
-        bigB = ent
-        marked = False
-        for maybeEnt in file[i]['nounChunks']:
-          if ent in maybeEnt:
-            if bigB in maybeEnt:
-              if len(maybeEnt.split()) - len(ent.split()) < 5:
-                if len(maybeEnt.split()) > len(bigB.split()):
-                  bigB = maybeEnt
-                  marked = True
-        if marked:
-          cleanedUpEnts.append(bigB)
-    file[i]['cleanedUpEnts'] = cleanedUpEnts
+      cEnts = mergeEntsFromNounChunks(ent, nounChunks, 0)
+      for cEnt in cEnts:
+        cleanedUpEnts.add(cEnt)
+    file[i]['cleanedUpEnts'] = list(cleanedUpEnts)
+  
+  # Write Data 
+  
   with open('./' + file_name, 'w') as handle:
     json.dump(file,handle)
 
--- enitityRecognition/src/tagAllData.py
+++ enitityRecognition/src/tagAllData.py
@@ -4,12 +4,15 @@ import spacy
 
 from tqdm import tqdm
 
+# Load Data 
 file_path = './../../data/HTML_TO_TEXT_UNCLEANED/'
 output_file_path = './../outputs/nerOutputs/'
 files = os.listdir(file_path)
 
+# Load Spacy NER model 
 nlp = spacy.load('./../models/sebi_ib')
 
+# Iterate thorugh the files and tag data 
 for file in tqdm(files):
     with open(file_path+file) as handle:
         data = handle.read().splitlines()
@@ -27,6 +30,8 @@ for file in tqdm(files):
         output[lineNumber]['ents'] = ents
         output[lineNumber]['ent_labels'] = entlabels
         lineNumber += 1
+
+    # Write data to file 
     file_name = output_file_path + file[:-7] + 'json'
     with open(file_name,'w') as handle:
         json.dump(output, handle)
